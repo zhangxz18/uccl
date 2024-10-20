5869e6f252a2b44e3dd0472b0ea5cdc027c4f3c1
YangZhou1997 <yangzhou.rpc@gmail.com>
2024-10-20 17:24:07 +0000

    add zhongjie's afxdp setup and use programs

--- afxdp/afxdp_setup_main.cc
+++ afxdp/afxdp_setup_main.cc
@@ -1,25 +1,28 @@
+#include <assert.h>
+#include <fcntl.h>
 #include <linux/if_xdp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/socket.h>
-#include <sys/stat.h>  // For chmod()
+#include <sys/stat.h>
 #include <sys/un.h>
 #include <unistd.h>
 #include <xdp/xsk.h>
 
+#define IF_NAME "ens6"
+#define SHM_NAME "UMEM_SHM"
+#define QUEUE_ID 0
+#define FILL_RING_SIZE                  \
+    (XSK_RING_PROD__DEFAULT_NUM_DESCS * \
+     2)  // recommened to be RX_RING_SIZE + NIC RING SIZE
+#define COMP_RING_SIZE XSK_RING_CONS__DEFAULT_NUM_DESCS
+#define TX_RING_SIZE XSK_RING_PROD__DEFAULT_NUM_DESCS
+#define RX_RING_SIZE XSK_RING_CONS__DEFAULT_NUM_DESCS
+
 #define SOCKET_PATH "/tmp/privileged_socket"
 #define FRAME_SIZE XSK_UMEM__DEFAULT_FRAME_SIZE
-#define NUM_FRAMES 4096
-
-void set_socket_permissions(const char *path) {
-    // Set the permissions of the UNIX domain socket to allow non-privileged
-    // access
-    if (chmod(path, 0666) == -1) {  // Permissions: Read/Write for everyone
-        perror("chmod");
-        exit(EXIT_FAILURE);
-    }
-}
+#define NUM_FRAMES (64 * 4096)
 
 struct xsk_umem *umem;
 struct xsk_ring_prod fill_ring;
@@ -28,51 +31,84 @@ struct xsk_socket *xsk;
 struct xsk_ring_cons rx_ring;
 struct xsk_ring_prod tx_ring;
 
-void send_fd(int unix_sock, int fd) {
-    struct msghdr msg = {0};
+int send_fd(int sockfd, int fd) {
+    assert(sockfd >= 0);
+    assert(fd >= 0);
+    struct msghdr msg;
     struct cmsghdr *cmsg;
-    char dummy_data[64];
-    char buf[CMSG_SPACE(sizeof(fd))];  // Allocate space for FD and control data
-    struct iovec io = {.iov_base = (void *)dummy_data,
-                       .iov_len = 2};  // Dummy data for iovec
-
-    // Zero out the control message buffer
+    struct iovec iov;
+    char buf[CMSG_SPACE(sizeof(fd))];
+    memset(&msg, 0, sizeof(msg));
     memset(buf, 0, sizeof(buf));
+    const char *name = "fd";
+    iov.iov_base = (void *)name;
+    iov.iov_len = 4;
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
 
-    // Set up the msghdr structure
-    msg.msg_iov = &io;      // I/O vector for message body (dummy data here)
-    msg.msg_iovlen = 1;     // One element in the I/O vector
-    msg.msg_control = buf;  // Control message buffer
-    msg.msg_controllen = sizeof(buf);  // Size of control message buffer
+    msg.msg_control = buf;
+    msg.msg_controllen = sizeof(buf);
 
-    // Set up the cmsghdr structure
-    cmsg = CMSG_FIRSTHDR(&msg);  // Initialize the first control message header
-    cmsg->cmsg_level = SOL_SOCKET;          // Specify the socket level
-    cmsg->cmsg_type = SCM_RIGHTS;           // Specify the type (passing FD)
-    cmsg->cmsg_len = CMSG_LEN(sizeof(fd));  // Length of the control message
+    cmsg = CMSG_FIRSTHDR(&msg);
 
-    // Copy the file descriptor into the control message data
-    memcpy(CMSG_DATA(cmsg), &fd, sizeof(fd));
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_RIGHTS;
+    cmsg->cmsg_len = CMSG_LEN(sizeof(fd));
 
-    // Send the message, including the file descriptor
-    if (sendmsg(unix_sock, &msg, 0) == -1) {
-        perror("sendmsg");
-        exit(EXIT_FAILURE);
+    *((int *)CMSG_DATA(cmsg)) = fd;
+
+    msg.msg_controllen = CMSG_SPACE(sizeof(fd));
+
+    if (sendmsg(sockfd, &msg, 0) < 0) {
+        fprintf(stderr, "sendmsg failed\n");
+        return -1;
     }
+    return 0;
+}
+
+void *create_shm(size_t size) {
+    int fd;
+    void *addr;
+
+    /* unlink it if we exit excpetionally before */
+    shm_unlink(SHM_NAME);
+
+    fd = shm_open(SHM_NAME, O_CREAT | O_RDWR | O_EXCL, 0666);
+    if (fd == -1) {
+        perror("shm_open");
+        return MAP_FAILED;
+    }
+
+    if (ftruncate(fd, size) == -1) {
+        perror("ftruncate");
+        return MAP_FAILED;
+    }
+
+    addr = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        return MAP_FAILED;
+    }
+
+    return addr;
+}
+
+void destroy_shm(void *addr, size_t size) {
+    munmap(addr, size);
+    shm_unlink(SHM_NAME);
 }
 
 int main() {
     int server_sock, client_sock;
     struct sockaddr_un addr;
-    void *umem_area;
     uint64_t umem_size = NUM_FRAMES * FRAME_SIZE;
-    struct xsk_umem_config umem_cfg = {.fill_size = NUM_FRAMES,
-                                       .comp_size = NUM_FRAMES,
+    struct xsk_umem_config umem_cfg = {.fill_size = FILL_RING_SIZE,
+                                       .comp_size = COMP_RING_SIZE,
                                        .frame_size = FRAME_SIZE,
                                        .frame_headroom = 0,
                                        .flags = 0};
-    struct xsk_socket_config xsk_cfg = {.rx_size = 2048,
-                                        .tx_size = 2048,
+    struct xsk_socket_config xsk_cfg = {.rx_size = RX_RING_SIZE,
+                                        .tx_size = TX_RING_SIZE,
                                         .libbpf_flags = 0,
                                         .xdp_flags = 0,
                                         .bind_flags = XDP_USE_NEED_WAKEUP};
@@ -85,56 +121,73 @@ int main() {
     memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
     strcpy(addr.sun_path, SOCKET_PATH);
+
+    mode_t old_mask = umask(0);  // set directory priviledge
+
     unlink(SOCKET_PATH);
     if (bind(server_sock, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    // Set the permissions on the UNIX domain socket file
-    set_socket_permissions(SOCKET_PATH);
+    umask(old_mask);  // restore
 
     if (listen(server_sock, 5) == -1) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
-
     printf("Waiting for non-privileged process to connect...\n");
     if ((client_sock = accept(server_sock, NULL, NULL)) == -1) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    printf("Connected!\n");
-
-    // Allocate UMEM and create AF_XDP socket
-    umem_area = mmap(NULL, umem_size, PROT_READ | PROT_WRITE,
-                     MAP_SHARED | MAP_ANONYMOUS | MAP_POPULATE, -1, 0);
+    // Step1: prepare a large shared memory for UMEM
+    old_mask = umask(0);  // set directory priviledge
+    void *umem_area = NULL;
+    umem_area = create_shm(umem_size);
     if (umem_area == MAP_FAILED) {
         perror("mmap");
-        exit(EXIT_FAILURE);
+        goto out;
     }
+    umask(old_mask);  // restore
+
+    // Step2: create UMEM
     if (xsk_umem__create(&umem, umem_area, umem_size, &fill_ring, &comp_ring,
                          &umem_cfg)) {
         perror("xsk_umem__create");
-        exit(EXIT_FAILURE);
+        goto out;
     }
-    if (xsk_socket__create(&xsk, "ens6", 0, umem, &rx_ring, &tx_ring,
+    // Step3: create a AF_XDP socket and bind it to a NIC queue and the UMEM
+    if (xsk_socket__create(&xsk, IF_NAME, QUEUE_ID, umem, &rx_ring, &tx_ring,
                            &xsk_cfg)) {
         perror("xsk_socket__create");
-        exit(EXIT_FAILURE);
+        goto out;
     }
 
-    printf("sending fds!\n");
-    // Send the file descriptors for the AF_XDP socket and UMEM
-    send_fd(client_sock, xsk_socket__fd(xsk));  // Send AF_XDP socket FD
-    send_fd(client_sock, xsk_umem__fd(umem));   // Send UMEM FD
+    /* Note: Actually, xsk_socket__fd(xsk) == xsk_umem__fd(umem), there is no
+     * need to send both. We send both of them just for demonstration. This is
+     * because libxdp would reuse the UMEM's fd for the ***first socket***
+     * binding to the UMEM, the UMEM fd is also created by socket(AF_XDP,
+     * SOCK_RAW, 0). See xsk_socket__create_shared()
+     */
+    assert(xsk_socket__fd(xsk) == xsk_umem__fd(umem));
 
-    close(client_sock);
-    close(server_sock);
+    // Step4: send the file descriptors for the AF_XDP socket and UMEM
+    if (send_fd(client_sock, xsk_socket__fd(xsk))) goto out;
+    if (send_fd(client_sock, xsk_umem__fd(umem))) goto out;
 
-    while (true) {
+    while (1) {
         sleep(1);
     }
+
+out:
+    if (umem_area != MAP_FAILED) {
+        destroy_shm(umem_area, umem_size);
+    }
+    close(client_sock);
+    close(server_sock);
+    unlink(SOCKET_PATH);
+
     return 0;
 }
