96fd3ca57fc2786f8432ff3ca7e8cc3292401853
YangZhou1997 <yangzhou.rpc@gmail.com>
2024-11-11 20:57:40 +0000

    nits

--- afxdp/run_nccl_test.sh
+++ afxdp/run_nccl_test.sh
@@ -9,29 +9,31 @@ LIBNCCL_PATH="${UCCL_HOME}/nccl/build/lib/libnccl.so"
 PROG_NAME=all_reduce_perf
 
 # PLUGIN_PATH="${UCCL_HOME}/nccl/ext-net/google-fastsocket/libnccl-net.so"
-# PLUGIN_PATH="/opt/aws-ofi-nccl/lib/libnccl-net.so"
 
-# mpirun -np 2 -N 1 --host 172.31.16.220,172.31.30.246 \
-#     --mca orte_base_help_aggregate 0 \
-#     --mca btl_tcp_if_include ens5 \
-#     -x LD_PRELOAD="${LIBNCCL_PATH} ${PLUGIN_PATH}" \
-#     -x NCCL_DEBUG=INFO \
-#     ${UCCL_HOME}/nccl-tests/build/${PROG_NAME} \
-#     -b 1024 -e 1048576 -f 2 -g 1 -w 100
-
-PLUGIN_PATH="${UCCL_HOME}/afxdp/libnccl-net.so"
+PLUGIN_PATH="/opt/aws-ofi-nccl/lib/libnccl-net.so"
 
 mpirun -np 2 -N 1 --host 172.31.16.220,172.31.30.246 \
     --mca orte_base_help_aggregate 0 \
     --mca btl_tcp_if_include ens5 \
     -x LD_PRELOAD="${LIBNCCL_PATH} ${PLUGIN_PATH}" \
     -x NCCL_DEBUG=INFO \
-    -x GLOG_logtostderr=0 \
-    -x NCCL_SOCKET_NTHREADS=1 \
-    -x NCCL_NSOCKS_PERTHREAD=1 \
-    -x NCCL_MAX_NCHANNELS=1 \
     ${UCCL_HOME}/nccl-tests/build/${PROG_NAME} \
     -b 1024 -e 1048576 -f 2 -g 1 -w 1000 -n 1000
+
+# PLUGIN_PATH="${UCCL_HOME}/afxdp/libnccl-net.so"
+
+# mpirun -np 2 -N 1 --host 172.31.16.220,172.31.30.246 \
+#     --mca orte_base_help_aggregate 0 \
+#     --mca btl_tcp_if_include ens5 \
+#     -x LD_PRELOAD="${LIBNCCL_PATH} ${PLUGIN_PATH}" \
+#     -x NCCL_DEBUG=INFO \
+#     -x GLOG_logtostderr=0 \
+#     -x NCCL_SOCKET_NTHREADS=1 \
+#     -x NCCL_NSOCKS_PERTHREAD=1 \
+#     -x NCCL_MAX_NCHANNELS=1 \
+#     ${UCCL_HOME}/nccl-tests/build/${PROG_NAME} \
+#     -b 1024 -e 1048576 -f 2 -g 1 -w 1000 -n 1000
+
 
 #    -x NCCL_P2P_NET_CHUNKSIZE=16777216 \
 #    -x NCCL_BUFFSIZE=16777216 \
