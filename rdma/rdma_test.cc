24bd7c4c3efcb681ba794a6479395f443b9fd4b6
chenzj <chenzj@node-0.chenzj-236845.lambda-mpi-pg0.utah.cloudlab.us>
2024-12-23 02:16:57 -0700

    Add multiple flows in rdma_test.

--- rdma/rdma_test.cc
+++ rdma/rdma_test.cc
@@ -29,6 +29,7 @@ DEFINE_bool(server, false, "Whether this is a server receiving traffic.");
 DEFINE_string(serverip, "", "Server IP address the client tries to connect.");
 DEFINE_string(perftype, "basic", "Performance type: basic/lat/tpt.");
 DEFINE_bool(warmup, false, "Whether to run warmup.");
+DEFINE_uint32(nflow, 1, "Number of flows.");
 DEFINE_uint32(nmsg, 8, "Number of messages within one request to post.");
 DEFINE_uint32(nreq, 4, "Outstanding requests to post.");
 DEFINE_uint32(msize, 65536, "Size of message.");
@@ -126,91 +127,94 @@ static void client_lat(RDMAEndpoint &ep, ConnID conn_id, void *data)
     }
 }
 
-static void server_tpt(RDMAEndpoint &ep, ConnID conn_id, void *data)
+static void server_tpt(RDMAEndpoint &ep, std::vector<ConnID> &conn_ids, std::vector<void *> &datas)
 {
-    if (FLAGS_warmup) {
-        for (int i = 0; i < 1000; i++) {
-            size_t len = 100;
-            void *recv_data = data;
-            auto *poll_ctx = ep.uccl_recv_async(conn_id, &recv_data, &len, 1);
-            ep.uccl_poll(poll_ctx);
-        }
+    FLAGS_iterations *= FLAGS_nflow;
+
+    size_t len[FLAGS_nflow][FLAGS_nreq][FLAGS_nmsg];
+    void *recv_data[FLAGS_nflow][FLAGS_nreq][FLAGS_nmsg];
+    std::vector<std::vector<PollCtx *>> poll_ctx_vec(FLAGS_nflow);
+    for (int i = 0; i < FLAGS_nreq; i++) {
+        poll_ctx_vec[i].resize(FLAGS_nmsg);
+    }
+    
+    for (int f = 0; f < FLAGS_nflow; f++) {
+        for (int r = 0; r < FLAGS_nreq; r++)
+            for (int m = 0; m < FLAGS_nmsg; m++) {
+                // First half of the buffer
+                len[f][r][m] = FLAGS_msize;
+                recv_data[f][r][m] = reinterpret_cast<char*>(datas[f]) + r * FLAGS_msize * FLAGS_nmsg + m * FLAGS_msize;
+            }
     }
 
-    size_t len[FLAGS_nreq][FLAGS_nmsg];
-    void *recv_data[FLAGS_nreq][FLAGS_nmsg];
-    std::vector<PollCtx *> poll_ctx_vec(FLAGS_nreq);
-    for (int r = 0; r < FLAGS_nreq; r++)
-        for (int m = 0; m < FLAGS_nmsg; m++) {
-            // First half of the buffer
-            len[r][m] = FLAGS_msize;
-            recv_data[r][m] = reinterpret_cast<char*>(data) + r * FLAGS_msize * FLAGS_nmsg + m * FLAGS_msize;
+    for (int f = 0; f < FLAGS_nflow; f++) {
+        for (int r = 0; r < FLAGS_nreq; r++) {
+            poll_ctx_vec[f][r] = ep.uccl_recv_async(conn_ids[f], recv_data[f][r], len[f][r], FLAGS_nmsg);
+            FLAGS_iterations--;
         }
-
-    for (int r = 0; r < FLAGS_nreq; r++) {
-        poll_ctx_vec[r] = ep.uccl_recv_async(conn_id, recv_data[r], len[r], FLAGS_nmsg);
-        FLAGS_iterations--;
     }
 
     while (FLAGS_iterations) {
-        for (int r = 0; r < FLAGS_nreq; r++) {
-            if (ep.uccl_poll_once(poll_ctx_vec[r])) {
-                poll_ctx_vec[r] = ep.uccl_recv_async(conn_id, recv_data[r], len[r], FLAGS_nmsg);
-                FLAGS_iterations--;
+        for (int f = 0; f < FLAGS_nflow; f++) {
+            for (int r = 0; r < FLAGS_nreq; r++) {
+                if (ep.uccl_poll_once(poll_ctx_vec[f][r])) {
+                    poll_ctx_vec[f][r] = ep.uccl_recv_async(conn_ids[f], recv_data[f][r], len[f][r], FLAGS_nmsg);
+                    FLAGS_iterations--;
+                }
             }
         }
     }
 }
 
-static void client_tpt(RDMAEndpoint &ep, ConnID conn_id, void *data)
+static void client_tpt(RDMAEndpoint &ep, std::vector<ConnID> &conn_ids, std::vector<void *> &datas)
 {
     volatile uint64_t prev_sec_bytes = 0;
     volatile uint64_t cur_sec_bytes = 0;
+
+    FLAGS_iterations *= FLAGS_nflow;
     
     // Create a thread to print throughput every second
     std::thread t([&] {
         while (!quit) {
             std::this_thread::sleep_for(std::chrono::seconds(1));
-            std::cout << "Throughput: " << std::fixed << std::setprecision(2) << 
+            std::cout << "(" << FLAGS_nflow << " flows) Throughput: " << std::fixed << std::setprecision(2) << 
                 (cur_sec_bytes - prev_sec_bytes) * 8.0 / 1000 / 1000 / 1000 << " Gbps" << std::endl;
             prev_sec_bytes = cur_sec_bytes;
         }
     });
-
-    if (FLAGS_warmup) {
-        for (int i = 0; i < 1000; i++) {
-            void *send_data = data;
-            auto *poll_ctx = ep.uccl_send_async(conn_id, send_data, 100);
-            ep.uccl_poll(poll_ctx);
-        }
-    }
     
-    std::vector<std::vector<PollCtx *>> poll_ctx_vec(FLAGS_nreq);
-    for (int i = 0; i < FLAGS_nreq; i++) {
-        poll_ctx_vec[i].resize(FLAGS_nmsg);
-    }
-
-    for (int r = 0; r < FLAGS_nreq; r++) {
-        for (int n = 0; n < FLAGS_nmsg; n++) {
-            void *send_data = reinterpret_cast<char*>(data) + r * FLAGS_msize * FLAGS_nmsg + n * FLAGS_msize;
-            poll_ctx_vec[r][n] = ep.uccl_send_async(conn_id, send_data, FLAGS_msize);
-            cur_sec_bytes += FLAGS_msize;
+    std::vector<std::vector<std::vector<PollCtx *>>> poll_ctx_vec(FLAGS_nflow);
+    for (int f = 0; f < FLAGS_nflow; f++) {
+        poll_ctx_vec[f].resize(FLAGS_nreq);
+        for (int r = 0; r < FLAGS_nreq; r++) {
+            poll_ctx_vec[f][r].resize(FLAGS_nmsg);
         }
-        FLAGS_iterations--;
+    }
+    for (int f = 0; f < FLAGS_nflow; f++) {
+        for (int r = 0; r < FLAGS_nreq; r++) {
+            for (int n = 0; n < FLAGS_nmsg; n++) {
+                void *send_data = reinterpret_cast<char*>(datas[f]) + r * FLAGS_msize * FLAGS_nmsg + n * FLAGS_msize;
+                poll_ctx_vec[f][r][n] = ep.uccl_send_async(conn_ids[f], send_data, FLAGS_msize);
+                cur_sec_bytes += FLAGS_msize;
+            }
+            FLAGS_iterations--;
+        }
     }
 
     int fin_msg = 0;
 
     while (FLAGS_iterations) {
-        for (int r = 0; r < FLAGS_nreq; r++) {
-            for (int n = 0; n < FLAGS_nmsg; n++) {
-                if (ep.uccl_poll_once(poll_ctx_vec[r][n])) {
-                    void *send_data = reinterpret_cast<char*>(data) + r * FLAGS_msize * FLAGS_nmsg + n * FLAGS_msize;
-                    poll_ctx_vec[r][n] = ep.uccl_send_async(conn_id, send_data, FLAGS_msize);
-                    cur_sec_bytes += FLAGS_msize;
-                    if (++fin_msg == FLAGS_nreq) {
-                        FLAGS_iterations--;
-                        fin_msg = 0;
+        for (int f = 0; f < FLAGS_nflow; f++) {
+            for (int r = 0; r < FLAGS_nreq; r++) {
+                for (int n = 0; n < FLAGS_nmsg; n++) {
+                    if (ep.uccl_poll_once(poll_ctx_vec[f][r][n])) {
+                        void *send_data = reinterpret_cast<char*>(datas[f]) + r * FLAGS_msize * FLAGS_nmsg + n * FLAGS_msize;
+                        poll_ctx_vec[f][r][n] = ep.uccl_send_async(conn_ids[f], send_data, FLAGS_msize);
+                        cur_sec_bytes += FLAGS_msize;
+                        if (++fin_msg == FLAGS_nreq) {
+                            FLAGS_iterations--;
+                            fin_msg = 0;
+                        }
                     }
                 }
             }
@@ -226,21 +230,26 @@ static void server_worker(void)
     std::string remote_ip;
     auto ep = RDMAEndpoint(GID_INDEX_LIST, NUM_DEVICES, NUM_ENGINES, ENGINE_CPU_START);
 
-    auto conn_id = ep.uccl_accept(0, remote_ip);
+    std::vector<ConnID> conn_ids;
+    std::vector<void *> datas;
 
-    printf("Server accepted connection from %s\n", remote_ip.c_str());
+    for (int i = 0; i < FLAGS_nflow; i++) {
+        auto conn_id = ep.uccl_accept(0, remote_ip);
+        printf("Server accepted connection from %s (flow#%d)\n", remote_ip.c_str(), i);
+        void *data = mmap(nullptr, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        assert(data != MAP_FAILED);
+        ep.uccl_regmr(conn_id, data, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, 0);
 
-    void *data = mmap(nullptr, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    assert(data != MAP_FAILED);
-
-    ep.uccl_regmr(conn_id, data, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, 0);
+        conn_ids.push_back(conn_id);
+        datas.push_back(data);
+    }
 
     if (FLAGS_perftype == "basic") {
-        server_basic(ep, conn_id, data);
+        server_basic(ep, conn_ids[0], datas[0]);
     } else if (FLAGS_perftype == "lat") {
-        server_lat(ep, conn_id, data);
+        server_lat(ep, conn_ids[0], datas[0]);
     } else if (FLAGS_perftype == "tpt") {
-        server_tpt(ep, conn_id, data);
+        server_tpt(ep, conn_ids, datas);
     } else {
         std::cerr << "Unknown performance type: " << FLAGS_perftype << std::endl;
     }
@@ -249,36 +258,44 @@ static void server_worker(void)
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
-    ep.uccl_deregmr(conn_id);
-    munmap(data, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg);
+    for (int i = 0; i < FLAGS_nflow; i++) {
+        ep.uccl_deregmr(conn_ids[i]);
+        munmap(datas[i], FLAGS_msize * FLAGS_nreq * FLAGS_nmsg);
+    }
 }
 
 static void client_worker(void)
 {
     auto ep = RDMAEndpoint(GID_INDEX_LIST, NUM_DEVICES, NUM_ENGINES, ENGINE_CPU_START);
 
-    auto conn_id = ep.uccl_connect(0, FLAGS_serverip);
+    std::vector<ConnID> conn_ids;
+    std::vector<void *> datas;
 
-    printf("Client connected to %s\n", FLAGS_serverip.c_str());
-    
-    void *data = mmap(nullptr, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-    assert(data != MAP_FAILED);
+    for (int i = 0; i < FLAGS_nflow; i++) {
+        auto conn_id = ep.uccl_connect(0, FLAGS_serverip);
+        printf("Client connected to %s (flow#%d)\n", FLAGS_serverip.c_str(), i);
+        void *data = mmap(nullptr, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        assert(data != MAP_FAILED);
+        ep.uccl_regmr(conn_id, data, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, 0);
 
-    ep.uccl_regmr(conn_id, data, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg, 0);
+        conn_ids.push_back(conn_id);
+        datas.push_back(data);
+    }
 
     if (FLAGS_perftype == "basic") {
-        client_basic(ep, conn_id, data);
+        client_basic(ep, conn_ids[0], datas[0]);
     } else if (FLAGS_perftype == "lat") {
-        client_lat(ep, conn_id, data);
+        client_lat(ep, conn_ids[0], datas[0]);
     } else if (FLAGS_perftype == "tpt") {
-        client_tpt(ep, conn_id, data);
+        client_tpt(ep, conn_ids, datas);
     } else {
         std::cerr << "Unknown performance type: " << FLAGS_perftype << std::endl;
     }
 
-    ep.uccl_deregmr(conn_id);
-
-    munmap(data, FLAGS_msize * FLAGS_nreq * FLAGS_nmsg);
+    for (int i = 0; i < FLAGS_nflow; i++) {
+        ep.uccl_deregmr(conn_ids[i]);
+        munmap(datas[i], FLAGS_msize * FLAGS_nreq * FLAGS_nmsg);
+    }
 }
 
 int main(int argc, char* argv[]) {
