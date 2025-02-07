8b17911ae7fcb1e67fdb873ad75d6d4537169c68
YangZhou1997 <yangzhou.rpc@gmail.com>
2025-02-07 03:23:55 +0000

    add plugin_path for p3d and p4d

--- aws_efa/run_nccl_test.sh
+++ aws_efa/run_nccl_test.sh
@@ -1,18 +1,20 @@
 # !/bin/bash
 
-mpirun --bind-to none --output-filename output.log -np 2 -N 1 --host 172.31.37.118,172.31.45.5,172.31.43.3,172.31.46.175 \
+# PLUGIN_PATH="/opt/aws-ofi-nccl/lib/libnccl-net.so" # for p4d
+PLUGIN_PATH="/opt/amazon/ofi-nccl/lib/x86_64-linux-gnu/libnccl-net.so" # for p3d
+
+mpirun --bind-to none -np 1 -N 1 --host 172.31.33.227 \
     --mca plm_rsh_args "-o StrictHostKeyChecking=no" \
     --mca orte_base_help_aggregate 0 \
     -x LD_PRELOAD="/opt/uccl/nccl/build/lib/libnccl.so" \
-    -x NCCL_NET_PLUGIN="/opt/aws-ofi-nccl/lib/libnccl-net.so" \
+    -x NCCL_NET_PLUGIN="${PLUGIN_PATH}" \
     -x NCCL_DEBUG=INFO \
+    -x NCCL_P2P_DISABLE=1 \
+    -x NCCL_SHM_DISABLE=1 \
+    -x NCCL_NET_DISABLE=0 \
     /opt/uccl/nccl-tests/build/alltoall_perf \
     -b 1K -e 1G -f 2 -g 1 -t 8
-
     # -w 100 -n 100
-    # -x NCCL_P2P_DISABLE=1 \
-    # -x NCCL_SHM_DISABLE=1 \
-    # -x NCCL_NET_DISABLE=0 \
     # -x NCCL_SOCKET_NTHREADS=4 \
     # -x NCCL_NSOCKS_PERTHREAD=2 \
     # -x NCCL_MAX_NCHANNELS=8 \
@@ -26,7 +28,7 @@ mpirun --bind-to none --output-filename output.log -np 2 -N 1 --host 172.31.37.1
 #         --mca plm_rsh_args "-o StrictHostKeyChecking=no" \
 #         --mca orte_base_help_aggregate 0 \
 #         -x LD_PRELOAD="/opt/uccl/nccl/build/lib/libnccl.so" \
-#         -x NCCL_NET_PLUGIN="/opt/aws-ofi-nccl/lib/libnccl-net.so" \
+#         -x NCCL_NET_PLUGIN="${PLUGIN_PATH}" \
 #         /opt/uccl/nccl-tests/build/alltoall_perf \
 #         -b 1K -e 1G -f 2 -g 1 -t 8
 # done
@@ -37,7 +39,7 @@ mpirun --bind-to none --output-filename output.log -np 2 -N 1 --host 172.31.37.1
 #         --mca plm_rsh_args "-o StrictHostKeyChecking=no" \
 #         --mca orte_base_help_aggregate 0 \
 #         -x LD_PRELOAD="/opt/uccl/nccl/build/lib/libnccl.so" \
-#         -x NCCL_NET_PLUGIN="/opt/aws-ofi-nccl/lib/libnccl-net.so" \
+#         -x NCCL_NET_PLUGIN="${PLUGIN_PATH}" \
 #         -x NCCL_P2P_DISABLE=1 \
 #         -x NCCL_SHM_DISABLE=1 \
 #         -x NCCL_NET_DISABLE=0 \
@@ -51,7 +53,7 @@ mpirun --bind-to none --output-filename output.log -np 2 -N 1 --host 172.31.37.1
 #         --mca plm_rsh_args "-o StrictHostKeyChecking=no" \
 #         --mca orte_base_help_aggregate 0 \
 #         -x LD_PRELOAD="/opt/uccl/nccl/build/lib/libnccl.so" \
-#         -x NCCL_NET_PLUGIN="/opt/aws-ofi-nccl/lib/libnccl-net.so" \
+#         -x NCCL_NET_PLUGIN="${PLUGIN_PATH}" \
 #         -x NCCL_P2P_DISABLE=1 \
 #         -x NCCL_SHM_DISABLE=0 \
 #         -x NCCL_NET_DISABLE=0 \
@@ -66,7 +68,7 @@ mpirun --bind-to none --output-filename output.log -np 2 -N 1 --host 172.31.37.1
 #         --mca plm_rsh_args "-o StrictHostKeyChecking=no" \
 #         --mca orte_base_help_aggregate 0 \
 #         -x LD_PRELOAD="/opt/uccl/nccl/build/lib/libnccl.so" \
-#         -x NCCL_NET_PLUGIN="/opt/aws-ofi-nccl/lib/libnccl-net.so" \
+#         -x NCCL_NET_PLUGIN="${PLUGIN_PATH}" \
 #         -x NCCL_P2P_DISABLE=1 \
 #         -x NCCL_SHM_DISABLE=1 \
 #         -x NCCL_NET_DISABLE=0 \
