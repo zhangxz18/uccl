1f21aba8b6afdaccbd00f0d5aad95177b9ef475d
YangZhou1997 <yangzhou.rpc@gmail.com>
2025-03-14 07:44:19 +0000

    change constant rate to 6.7Gb/s, now scattered memcpy works almost the same as zc

--- efa/run_nccl_test_debug.sh
+++ efa/run_nccl_test_debug.sh
@@ -1,11 +1,16 @@
 # !/bin/bash
 
-TEST="uccl_rdma"
-# TEST="uccl_rdma_zc"
+# TEST="uccl_rdma"
+TEST="uccl_rdma_zc"
 
 LIBNCCL_PATH="/opt/${TEST}/nccl/build/lib/libnccl.so"
 PLUGIN_PATH="/opt/${TEST}/efa/libnccl-net.so"
 BIN_PATH="/opt/${TEST}/nccl-tests/build/alltoall_perf"
+
+COPY_CHANNELS=8
+if [ "$TEST" = "uccl_rdma_zc" ]; then
+    COPY_CHANNELS=2
+fi
 
 mpirun --bind-to none -np 1 -N 1 --host localhost \
     --tag-output --merge-stderr-to-stdout \
@@ -18,26 +23,20 @@ mpirun --bind-to none -np 1 -N 1 --host localhost \
     -x NCCL_SHM_DISABLE=1 \
     -x NCCL_NET_DISABLE=0 \
     -x CUDA_VISIBLE_DEVICES="0,1,2,3,4,5,6,7" \
-    -x NCCL_MAX_NCHANNELS=8 \
-    -x NCCL_MIN_NCHANNELS=8 \
-    -x NCCL_NCHANNELS_PER_NET_PEER=4 \
+    -x NCCL_MAX_NCHANNELS=${COPY_CHANNELS} \
+    -x NCCL_MIN_NCHANNELS=${COPY_CHANNELS} \
+    -x NCCL_P2P_NET_CHUNKSIZE=131072 \
+    -x NCCL_NCHANNELS_PER_NET_PEER=1 \
     -x NCCL_NET_GDR_LEVEL=SYS \
-    -x NCCL_P2P_NET_CHUNKSIZE=524288 \
     -x NCCL_BUFFSIZE=8388608 \
     -x GLOG_logtostderr=1 \
     -x NCCL_TOPO_FILE=/opt/uccl_rdma/efa/p4d-24xl-topo.xml \
     -x UCCL_ENGINE_QUIET=1 \
+    -x CUDA_MODULE_LOADING=EAGER \
     ${BIN_PATH} \
-    -b 1K -e 1G -f 2 -w 5 -n 100 -g 1 -t 2 \
+    -b 1K -e 1G -f 2 -w 5 -n 100 -g 1 -t 8 \
     >& alltoall_debug.log
 
-
-    # Test for ZC
-    # -x NCCL_MAX_NCHANNELS=8 \
-    # -x NCCL_MIN_NCHANNELS=8 \
-
-    # Only for uccl_rdma_zc + extra_launch
-    # -x CUDA_MODULE_LOADING=EAGER \
 
 # export LD_PRELOAD="${LIBNCCL_PATH} ${PLUGIN_PATH}"
 # export NCCL_DEBUG=INFO
@@ -55,10 +54,6 @@ mpirun --bind-to none -np 1 -N 1 --host localhost \
 # export UCCL_ENGINE_QUIET=1
 
 # ${BIN_PATH} -b 1M -e 1M -f 2 -w 0 -n 1 -g 1 -t 8
-
-# sudo -E /usr/local/cuda/bin/nsys profile --trace=cuda,nvtx,osrt --output=nccl_test_report \
-#     --gpu-metrics-device=0 
-
+# sudo -E /usr/local/cuda/bin/nsys profile --trace=cuda,nvtx,osrt --output=nccl_test_report --gpu-metrics-device=0 
 # compute-sanitizer --tool memcheck ${BIN_PATH} -b 1M -e 1M -f 2 -w 0 -n 1 -g 1 -t 2 >& alltoall_debug.log
-
 # gdb --args ${BIN_PATH} -b 1M -e 1M -f 2 -w 0 -n 1 -g 1 -t 2
