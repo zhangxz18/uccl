ef024bdeff01cf85b1a7e707d22303616058a007
zhongjiechen <chenzhongjie97@outlook.com>
2025-04-02 15:47:33 +0000

    Nits.

--- rdma/sync_repo.sh
+++ rdma/sync_repo.sh
@@ -2,24 +2,8 @@
 
 SOURCE_DIR="/home/azureuser/uccl_rdma/"
 
-TARGET_MACHINES=(
-  "74.179.81.170" 
-  "74.179.82.149" 
-  "74.179.81.10"
-  "74.179.82.138"
-  "128.85.41.27" 
-  "128.85.40.255" 
-  "128.85.41.120" 
-  "128.85.41.236" 
-  "128.85.40.216" 
-  "128.85.40.197" 
-  "128.85.41.81" 
-  "128.85.41.11" 
-  "128.85.41.55" 
-  "128.85.41.253" 
-  "128.85.40.243" 
-  "128.85.42.40"
-)
+# Read IPs from the 'ips' file into an array
+IFS=$'\n' read -d '' -r -a TARGET_MACHINES < "ips"
 
 TARGET_DIR="/home/azureuser/uccl_rdma/"
 
