a6748c00ab9e20089fe4b9bba4d6a8d1485fb85f
Ziming Mao <ziming.mao@berkeley.edu>
2025-06-11 21:44:34 -0400

    Create the uccl_home on other nodes if doesn't exist (#39)
    * check if local uccl_home exists, and create remote uccl_home if it doesnt
    
    * update gitignore

--- scripts/rsync.py
+++ scripts/rsync.py
@@ -1,5 +1,6 @@
 from shared import *
 import os
+import asyncio
 from pathlib import Path
 import argparse
 
@@ -11,16 +12,22 @@ UCCL_HOME = os.getenv("UCCL_HOME")
 if not UCCL_HOME:
     raise ValueError("UCCL_HOME environment variable is not set.")
 
+if not Path(UCCL_HOME).is_dir():
+    raise FileNotFoundError(f"Local directory {UCCL_HOME} does not exist.")
+
 print(f"Using UCCL_HOME: {UCCL_HOME}")
 
+async def prepare_remote(node):
+    await run_command(f"ssh -o StrictHostKeyChecking=no {node} 'mkdir -p {UCCL_HOME}'")
+
 async def rsync(nodes):
+    await asyncio.gather(*(prepare_remote(node) for node in nodes))
     tasks = [
         run_command(
-            f"rsync -auv -e 'ssh -o StrictHostKeyChecking=no' --mkpath --delete {UCCL_HOME}/ {node}:{UCCL_HOME}/"
+            f"rsync -auv -e 'ssh -o StrictHostKeyChecking=no' --delete {UCCL_HOME}/ {node}:{UCCL_HOME}/"
         )
         for node in nodes
     ]
-    # Run all tasks in parallel
     await asyncio.gather(*tasks)
 
 nodes = get_nodes(args.node_file)
