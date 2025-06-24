853704be17b81cea943d582f8dda569efded56bc
Ziming Mao <ziming.mao@berkeley.edu>
2025-06-24 10:42:26 -0400

    remove endif (#78)

--- rdma/transport.cc
+++ rdma/transport.cc
@@ -802,7 +802,6 @@ RDMAEndpoint::RDMAEndpoint(int num_engines_per_dev)
     // Create listening sockets
     create_listen_socket(&test_listen_fds_[i], kTestListenPort + i);
   }
-#endif
 }
 #endif
 
